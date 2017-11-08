c6d2b42d60a335d97da30322da6ad95f730830ce
yangxg <zmrenwu@163.com>
2017-11-08 23:31:57 +0800

    Hide sensitive data to environment variable.

--- blogproject/settings/local.py
+++ blogproject/settings/local.py
@@ -1,9 +1,7 @@
 from .common import *
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
-                            default='i3k%m-808v3_)^h7975iw4v&fl5chq41^19j@u+b*vx7dvw*q$')
-
 DEBUG = True
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default='i3k%m-808v3_)^h7975iw4v&fl5chq41^19j@u+b*vx7dvw*q$')
 
 # debug toolbar
 INTERNAL_IPS = ['127.0.0.1']
@@ -18,10 +16,3 @@ INSTALLED_APPS += [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# django anymail grid
-# ANYMAIL = {
-#     "SENDGRID_API_KEY": "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",
-#     # "MAILGUN_SENDER_DOMAIN": 'mg.example.com',
-# }
-# EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
-# DEFAULT_FROM_EMAIL = "zmrenwu_blog@zmrenwu.com"
--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -5,10 +5,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = ['.zmrenwu.com']
 DEBUG = False
 
-# django anymail grid
+# django anymail
 ANYMAIL = {
-    "SENDGRID_API_KEY": "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",
-    # "MAILGUN_SENDER_DOMAIN": 'mg.example.com',
+    "SENDGRID_API_KEY": os.environ.get('DJANGO_SENDGRID_API_KEY'),
 }
 EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
 DEFAULT_FROM_EMAIL = "zmrenwu_blog@zmrenwu.com"
